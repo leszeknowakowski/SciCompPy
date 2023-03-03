7c3453d319edec09a3daeb940ecab736e22979e2
leszeknowakowski <126915797+leszeknowakowski@users.noreply.github.com>
2023-03-03 23:20:51 +0100

    Delete hello.py

--- lesson1/hello.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-name = input("Enter your name: ")
-print ( "Hello {}!".format(name) )
-for char in name:
-    print(char, ord(char))   # ASCII code
-input("Press any key ...")   # on Windows
